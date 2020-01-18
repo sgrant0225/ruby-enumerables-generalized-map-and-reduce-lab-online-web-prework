f13b37b056a383b8628bc9b0ee3e307d86bfe85e
Sharnell Grant <sharnellgrant@gmail.com>
2020-01-18 23:55:31 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,22 @@
 # Your Code Here
+# def my_own_map(negative_array)
+# negative_array = []
+# i = 0 
+# while i < negative_array.map do |i| 
+#   i * -1
+#   negative_array << i
+# end 
+# i += 1
+# end 
+# negative_array
+
+def my_own_map(i)
+  array = []
+  total = i * array
+  yield(total)
+  total
+end
+
+my_own_map(-1) do |total|
+  return total
+end
